6dda2dd0e0aa2f03e4ee35e52dad67ba4863577d
Saurav Ganguly <hisauravganguly@gmail.com>
2024-02-25 16:16:19 +0530

    init

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:cpc_salary_calculator/main_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
