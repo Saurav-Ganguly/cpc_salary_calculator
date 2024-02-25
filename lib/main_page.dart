eb51b6f2d66671de829fdea0f3a2e64ae56b8d6c
Saurav Ganguly <hisauravganguly@gmail.com>
2024-02-25 17:08:08 +0530

    changed the gradient color

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -19,13 +19,17 @@ class _MainPageState extends State<MainPage> {
             height: MediaQuery.of(context).size.height * .33,
             decoration: const BoxDecoration(
               gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
-                  Color(0xFFB6185B),
-                  Color(0xFF851D92),
-                ],
-              ),
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  // colors: [
+                  //   Color(0xFFB6185B),
+                  //   Color(0xFF851D92),
+                  // ],
+                  colors: [
+                    Color.fromARGB(255, 177, 107, 2),
+                    Color.fromARGB(255, 252, 240, 240),
+                    Color.fromARGB(255, 89, 165, 92)
+                  ]),
             ),
             child: const Center(
               child: CircleAvatar(
